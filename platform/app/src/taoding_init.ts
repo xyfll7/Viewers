0340cafb47417dcd8faa710976c97c43327f3391
xyfll7 <l.7@qq.com>
2025-07-16 09:55:29 +0800

    fix: streamline token handling in taoding_init function

--- platform/app/src/taoding_init.ts
+++ platform/app/src/taoding_init.ts
@@ -8,14 +8,10 @@ export function taoding_init() {
   if (token) {
     // 设置token到localStorage
     localStorage.setItem('token', token);
-  } else {
+  } else if (process.env.NODE_ENV === 'production') {
     // 如果没有token，提示非法访问
     alert('非法访问');
-
-    if (process.env.NODE_ENV === 'development') {
-      window.location.href = 'http://127.0.0.1:8080/v/login';
-    } else {
-      window.location.href = 'http://47.115.206.154:8080/v/login';
-    }
+    window.location.href = 'http://47.115.206.154:8080/v/login';
+    // window.location.href = 'http://127.0.0.1:8080/v/login';
   }
 }
